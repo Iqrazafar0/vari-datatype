1a4e4ff2c3b537d6dd38274c2e50c50ae556f84f
Iqra Zafar <iqraraozafar0@gmail.com>
2024-08-06 13:46:59 +0500

    Update and rename prac1stprog.py.py to swappingnumbers.py.py

---
+++ swappingnumbers.py.py
@@ -0,0 +1,20 @@
+#Question 1: Write a Python program to swap two numbers using a temporary variable.
+
+#taking input from user
+a = int(input("Enter num 1:"))
+b = int(input("Enter num 2:"))
+#using a temp variable for swping
+temp = a 
+#store the value of a in temp
+a = b     
+#assign the value of b to a
+temp = b 
+#assign the value of temp(original a)to b
+#prinnt the swaped values
+print("Ater swaping: a=", a,"b =", b)
+
+
+
+
+
+
